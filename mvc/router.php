ae21770132ccb5818b9e5e8a318a4579a7a0b39a
Time Kliss Solutions <48053167+klisssolutions@users.noreply.github.com>
2019-03-11 11:05:19 -0300

    Add files via upload

--- mvc/router.php
+++ mvc/router.php
@@ -1,4 +1,20 @@
 <?php
 $controller = null;
 $modo = null;
+
+if(isset($_GET["controller"])){
+    //Sempre Serão enviadas pela view
+    $controller = strtoupper($_GET["controller"]);
+    $modo = strtoupper($_GET["modo"]);
+
+    switch($controller){
+        case "CONTATOS":
+            switch($modo){
+                case "INSERIR":
+                case "ATUALIZAR":
+                case "EXCLUIR":
+                case "BUSCAR":
+            }
+    }
+}
 ?>
